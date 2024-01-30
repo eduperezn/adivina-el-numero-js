9fc4e7d187881908c0499556d2faaddeea6f5228
eduperezn <edu.perezn.p@gmail.com>
2024-01-30 08:01:26 -0600

    Se agrega parrafo de instrucciones

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 let titulo = document.querySelector('h1');
+titulo.innerHTML = "Juego del número esecreto";
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Indica un número del 1 al 10";
