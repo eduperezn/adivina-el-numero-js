3597df62fbc5bee93f2d1d9a9936c0283b8cba3c
eduperezn <edu.perezn.p@gmail.com>
2024-02-03 13:25:41 -0600

    Se agrega funcion para generar numero secreto

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento,texto){
     let elementoHTML = document.querySelector(elemento);
-elementoHTML.innerHTML = texto;
+    elementoHTML.innerHTML = texto;
+    return;
 }
 
 function intentoDeUsuario(){
     alert("Click desde el boton");
+    return;
+}
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10+1);
 }
 
 asignarTextoElemento('h1','Juego del numero');
